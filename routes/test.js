f02bde28b96d0b854a85e4182d1691022897e3d8
Robert Vilcu <robertvilcu89@gmail.com>
2024-10-25 16:56:26 +0100

    chore: deleted all js files

--- routes/test.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as controller from '../controllers/test.js'
-
-const router = express.Router();
-
-router.get('/', controller.test);
-
-export default router;
