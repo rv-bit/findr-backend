6670331b67bf178fc9ccb680920907061ab19ba6
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-08 19:25:04 +0000

    chore: removed google from auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -20,11 +20,6 @@ export const auth = betterAuth({
     },
 
     socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            redirectURI: process.env.BASE_URL + "/api/auth/callback/google/",
-        },
         github: {
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
