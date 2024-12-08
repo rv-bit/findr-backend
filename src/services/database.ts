187d6788555566395fb90e6f4b689c2bbc384976
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-08 20:10:12 +0000

    chore: updated database configuration

--- src/services/database.ts
+++ src/services/database.ts
@@ -3,10 +3,11 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
 const poolConnection = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    database: process.env.MYSQLDATABASE,
+    password: process.env.MYSQLPASSWORD,
+    port: parseInt(process.env.MYSQLPORT || "3306"),
 });
 
 const db = drizzle({
