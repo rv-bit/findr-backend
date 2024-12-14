b13cd190e8819945e04e50861e7bbdb8c328200d
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-14 18:21:16 +0000

    chore: refactor origins

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,17 @@ import { auth, limiter } from './utils/index.js' // Your auth config
 import routes from './routes/index.js' // Your other routes
 
 const app = express()
+const trustedOrigins = process.env.BETTER_TRUSTED_ORIGINS?.split(',').map((origin) => {
+	return origin.startsWith('http') ? origin : `https://${origin}`
+})
 
 const corsOptions = {
-	origin: process.env.BETTER_TRUSTED_ORIGINS?.split(','),
+	origin: trustedOrigins,
 	credentials: true, // This ensures that cookies/credentials are allowed
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization'], // Ensure necessary headers are allowed
 }
 
-app.set('trust proxy', 1) // Trust first proxy
 app.use(cors(corsOptions))
 app.use(limiter) // Apply rate limiting for all routes
 
@@ -26,8 +28,10 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'docker') 
 	app.use(express.static(path.join(__dirname, '../dist')))
 }
 
-app.use('/api/v1', express.json(), routes)
 app.all('/api/auth/*', toNodeHandler(auth))
+
+app.use(express.json())
+app.use('/api/v1', routes)
 
 app.use((req, res, next) => {
 	res.status(404).send('Not Found')
