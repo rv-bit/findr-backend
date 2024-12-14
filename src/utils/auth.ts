250cecbe45ad9a022fb9a66f1d50000077670b9f
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-14 18:43:29 +0000

    chore: refactor redirectURI

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,33 +1,33 @@
-import "dotenv/config";
+import 'dotenv/config'
 
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 
-import * as schema from "../models/index.js";
-import db from "../services/database.js";
+import * as schema from '../models/index.js'
+import db from '../services/database.js'
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "mysql",
-        schema: schema
-    }),
+	database: drizzleAdapter(db, {
+		provider: 'mysql',
+		schema: schema,
+	}),
 
-    trustedOrigins: process.env.BETTER_TRUSTED_ORIGINS?.split(",") || ["http://localhost:3000"],
+	trustedOrigins: process.env.BETTER_TRUSTED_ORIGINS?.split(',') || ['http://localhost:3000'],
 
-    emailAndPassword: {
-        enabled: true,
-    },
+	emailAndPassword: {
+		enabled: true,
+	},
 
-    socialProviders: {
-        github: {
-            clientId: process.env.GITHUB_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-            redirectURI: process.env.NODE_ENV === "development" ? process.env.BASE_URL + "/api/auth/callback/github/" : process.env.RAILWAY_PUBLIC_DOMAIN + "/api/auth/callback/github/",
-        }
-    },
+	socialProviders: {
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+			redirectURI: process.env.NODE_ENV === 'development' ? process.env.BASE_URL + '/api/auth/callback/github/' : (process.env.RAILWAY_PUBLIC_DOMAIN?.startsWith('http') ? process.env.RAILWAY_PUBLIC_DOMAIN : `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`) + '/api/auth/callback/github/',
+		},
+	},
 
-    session: {
-        expiresIn: 60 * 60 * 24 * 7, // 7 days
-        updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
-    },
-});
+	session: {
+		expiresIn: 60 * 60 * 24 * 7, // 7 days
+		updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
+	},
+})
