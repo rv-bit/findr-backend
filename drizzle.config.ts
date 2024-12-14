c2de69d33936f309a1241b7c257b2e67c06554e2
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-14 15:50:35 +0000

    chore: refactor config for drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,19 @@
-import 'dotenv/config';
-import { defineConfig, Config } from 'drizzle-kit';
+import 'dotenv/config'
+import { defineConfig, Config } from 'drizzle-kit'
 
 export default defineConfig({
-    out: './drizzle',
-    schema: './dist/models/index.js',
-    dialect: 'mysql',
+	out: './drizzle',
+	schema: './dist/models/index.js',
+	dialect: 'mysql',
 
-    dbCredentials: {
-        host: process.env.DATABASE_HOST!,
-        user: process.env.DATABASE_USER!,
-        database: process.env.DATABASE_NAME!,
-        password: process.env.DATABASE_PASSWORD!,
-    },
+	dbCredentials: {
+		host: process.env.MYSQLHOST!,
+		user: process.env.MYSQLUSER!,
+		password: process.env.MYSQLPASSWORD!,
+		database: process.env.MYSQLDATABASE!,
+		port: parseInt(process.env.MYSQLPORT || '3306'),
+	},
 
-    verbose: true,
-    strict: true,
-} as Config);
+	verbose: true,
+	strict: true,
+} as Config)
