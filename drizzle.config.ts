95a5ddb5314f11ea147f10e53714940f3e81169a
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-14 15:53:00 +0000

    chore: refactor database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 		host: process.env.MYSQLHOST!,
 		user: process.env.MYSQLUSER!,
 		password: process.env.MYSQLPASSWORD!,
-		database: process.env.MYSQLDATABASE!,
+		database: process.env.MYSQL_DATABASE!,
 		port: parseInt(process.env.MYSQLPORT || '3306'),
 	},
 
--- src/services/database.ts
+++ src/services/database.ts
@@ -1,17 +1,17 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/mysql2'
+import mysql from 'mysql2/promise'
 
 const poolConnection = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    database: process.env.MYSQLDATABASE,
-    password: process.env.MYSQLPASSWORD,
-    port: parseInt(process.env.MYSQLPORT || "3306"),
-});
+	host: process.env.MYSQLHOST,
+	user: process.env.MYSQLUSER,
+	database: process.env.MYSQL_DATABASE,
+	password: process.env.MYSQLPASSWORD,
+	port: parseInt(process.env.MYSQLPORT || '3306'),
+})
 
 const db = drizzle({
-    client: poolConnection,
-});
+	client: poolConnection,
+})
 
-export default db;
+export default db
