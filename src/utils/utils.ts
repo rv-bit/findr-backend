456af08a37bd4518a0b5f9876bec146d99047168
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-17 16:36:57 +0000

    feat: add hopefully more security over xss

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,32 @@
 import { Request, Response, NextFunction } from "express";
-import { ObjectSchema } from "joi";
+import Joi, { ObjectSchema } from "joi";
+import xss from 'xss';
+
+const xssOptions = {
+    whiteList: {}, // empty, means filter out all tags
+    stripIgnoreTag: true, // filter out all HTML not in the whitelist
+    stripIgnoreTagBody: ['script'], // the script tag is a special case, we need to filter out its content
+}
+
+// Create a custom Joi extension to sanitize strings
+export const JoiXss = Joi.extend((joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.xss': '{{#label}} contains disallowed HTML content!',
+    },
+    rules: {
+        xss: {
+            validate(value, helpers) {
+                const clean = xss(value, xssOptions);
+                if (clean !== value) {
+                    return helpers.error('string.xss', { value });
+                }
+                return clean;
+            },
+        },
+    },
+}));
 
 export const validateRequest = (schema: ObjectSchema) => {
     return (req: Request, res: Response, next: NextFunction) => {
