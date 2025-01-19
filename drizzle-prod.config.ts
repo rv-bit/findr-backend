95dcf97809dfecca6294470231382248a86adc28
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-19 15:02:56 +0000

    chore: changed config

--- drizzle-prod.config.ts
+++ drizzle-prod.config.ts
@@ -3,8 +3,8 @@ import { defineConfig, Config } from 'drizzle-kit'
 
 export default defineConfig({
 	dialect: 'mysql',
-	out: './app/build/services/database/migrations',
-	schema: './app/build/services/database/schema.js',
+	out: './app/src/services/database/migrations',
+	schema: './app/src/services/database/schema.js',
 
 	dbCredentials: {
 		url: process.env.MYSQL_URL,
