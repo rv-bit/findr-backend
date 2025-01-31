ce7ed3ae554d84f22ec4fd37e585e4feedda41cb
Robert Vilcu <robertvilcu89@gmail.com>
2025-01-31 19:33:25 +0000

    chore: connect with pool instead of uri

--- src/services/database/database.ts
+++ src/services/database/database.ts
@@ -1,8 +1,16 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/mysql2'
+import { createPool } from 'mysql2'
+
+const pool = createPool({
+	host: process.env.MYSQLHOST,
+	user: process.env.MYSQLUSER,
+	password: process.env.MYSQLPASSWORD,
+	database: process.env.MYSQL_DATABASE,
+})
 
 const db = drizzle({
-	connection: {uri: process.env.MYSQL_URL},
+	client: pool,
 })
 
 export default db
