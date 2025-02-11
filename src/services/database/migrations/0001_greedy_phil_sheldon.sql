372bd017e3eefbd2d19d1e6d2b78d99adebbf1f3
Robert Vilcu <robertvilcu89@gmail.com>
2025-02-11 16:49:08 +0000

    chore: updated migrations

--- src/services/database/migrations/0001_greedy_phil_sheldon.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `session` ADD `impersonated_by` text;--> statement-breakpoint
-ALTER TABLE `user` ADD `role` text;--> statement-breakpoint
-ALTER TABLE `user` ADD `banned` boolean;--> statement-breakpoint
-ALTER TABLE `user` ADD `ban_reason` text;--> statement-breakpoint
-ALTER TABLE `user` ADD `ban_expires` timestamp;
--- src/services/database/migrations/0002_tranquil_makkari.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `user` ADD `about_description` text;--> statement-breakpoint
-ALTER TABLE `user` ADD `slug` text;
