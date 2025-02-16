d14b75519a8e0d3ae5099608f54243727fde1b46
Robert Vilcu <robertvilcu89@gmail.com>
2025-02-16 20:37:21 +0000

    chore: add drizzle prod connection

---
+++ drizzle-prod.config.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import { defineConfig, Config } from 'drizzle-kit'
+
+export default defineConfig({
+	dialect: 'mysql',
+	out: './src/services/database/migrations',
+	schema: './src/services/database/schema.ts',
+
+	dbCredentials: {
+		url: process.env.MYSQL_URL_PROD,
+	},
+
+	verbose: true,
+	strict: true,
+} as Config)
