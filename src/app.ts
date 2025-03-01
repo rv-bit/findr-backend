c7ab952f2cf9a468af705c614bec9a51062d884d
Robert Vilcu <robertvilcu89@gmail.com>
2025-03-01 20:54:02 +0000

    feat: add bun serve and default route

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,48 @@
 import 'dotenv/config'
-import express from 'express'
+import { Hono } from 'hono'
 
-import helmet from 'helmet'
-import cors from 'cors'
-import path from 'path'
+// import helmet from 'helmet'
+// import cors from 'cors'
+// import path from 'path'
 
-import { toNodeHandler } from 'better-auth/node'
-import { auth, limiter } from '~/utils/index'
+// import { toNodeHandler } from 'better-auth/node'
+// import { auth, limiter } from '~/utils/index'
 
-import routes from '~/routes/index'
-import * as middlewares from './middlewares'
+// import routes from '~/routes/index'
+// import * as middlewares from './middlewares'
 
-const app = express()
+const app = new Hono()
 const trustedOrigins = process.env.BETTER_TRUSTED_ORIGINS?.split(',').map((origin) => {
 	return origin.startsWith('http') ? origin : `https://${origin}`
 })
 
 console.log('Trusted Origins:', trustedOrigins)
 
-const corsOptions = {
-	origin: trustedOrigins,
-	credentials: true, // This ensures that cookies/credentials are allowed
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization'], // Ensure necessary headers are allowed
-}
+// const corsOptions = {
+// 	origin: trustedOrigins,
+// 	credentials: true, // This ensures that cookies/credentials are allowed
+// 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// 	allowedHeaders: ['Content-Type', 'Authorization'], // Ensure necessary headers are allowed
+// }
 
-app.set('trust proxy', 1) // Trust first proxy
-app.use(cors(corsOptions))
-app.use(helmet())
-app.use(limiter) // Apply rate limiting for all routes
+// app.set('trust proxy', 1) // Trust first proxy
+// app.use(cors(corsOptions))
+// app.use(helmet())
+// app.use(limiter) // Apply rate limiting for all routes
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'docker') {
-	const __dirname = path.resolve()
-	app.use(express.static(path.join(__dirname, '../dist')))
-}
+// if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'docker') {
+// 	const __dirname = path.resolve()
+// 	app.use(express.static(path.join(__dirname, '../dist')))
+// }
 
-app.all('/auth/*', toNodeHandler(auth))
+// app.all('/auth/*', toNodeHandler(auth))
 
-app.use(express.json())
-app.use('/v0/', routes)
+// app.use(express.json())
+// app.use('/v0/', routes)
 
-app.use(middlewares.notFoundHandler)
-app.use(middlewares.errorHandler)
+// app.use(middlewares.notFoundHandler)
+// app.use(middlewares.errorHandler)
+
+app.get('/', (c) => c.text('Hello Bun!'))
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Server } from 'http'
 import app from './app'
-
-const server = new Server(app)
 const PORT = process.env.PORT
 
-server.listen(PORT, () => {
-	console.log(`Server listening on ${PORT}`)
+Bun.serve({
+	fetch: app.fetch,
+	port: PORT,
 })
+
+console.log(`Server listening on ${PORT}`)
