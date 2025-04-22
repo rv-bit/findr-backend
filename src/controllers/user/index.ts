c13b41db927d70d416d7427eeeb14e3a764d16a7
Robert Vilcu <robertvilcu89@gmail.com>
2025-04-22 21:00:49 +0100

    fix: return status if no user found

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -11,17 +11,16 @@ export const getUserInfo = handler(async (req: Request, res: Response) => {
 
 	const username = slug.replace(/-/g, ' ')
 
-	const user = await db.select().from(schema.user).where(eq(schema.user.username, username)).limit(1).execute()
-
-	if (!user) {
-		res.status(401).json({
-			message: 'Failed to get user',
+	const users = await db.select().from(schema.user).where(eq(schema.user.username, username)).limit(1)
+	if (!users || users.length === 0) {
+		res.status(200).json({
+			data: null,
+			message: 'User not found',
 		})
 		return
 	}
 
-	const userInfo = user[0]
-
+	const userInfo = users[0]
 	res.status(200).json({
 		data: {
 			id: userInfo.id,
