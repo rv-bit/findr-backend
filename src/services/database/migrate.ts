c8aa83352ab95ca859d20fdef50eed52b5902c3f
Robert Vilcu <robertvilcu89@gmail.com>
2025-04-26 02:14:01 +0100

    chore: remove migrate, no use case for it

--- src/services/database/migrate.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres"
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
-
-const db = drizzle(process.env.MYSQL_URL);
-
-await migrate(db, {
-    migrationsFolder: "./src/services/database/migrations",
-});
