ec0249cfe16d5ddae5efef3fae1771bb086d94f4
Robert Vilcu <robertvilcu89@gmail.com>
2025-05-11 21:32:22 +0100

    chore: refactor to use the shared function instead

--- src/controllers/post/index.ts
+++ src/controllers/post/index.ts
@@ -6,11 +6,15 @@ import { nanoid } from 'nanoid'
 import * as schema from '~/services/database/schema'
 import db from '~/services/database/database'
 
-import { auth, handler } from '~/lib/index'
+import { auth, type Session } from '~/lib/auth'
+
+import { handler } from '~/lib/index'
 import logger from '~/lib/logger'
 
-import type { NewPostSchema } from '~/routes/post/schema'
 import type { PostResponse } from '~/lib/types/shared'
+import type { NewPostSchema } from '~/routes/post/schema'
+
+import { getPostByPostId } from '../shared/post'
 
 export const getAllPosts = handler(async (req: Request, res: Response) => {
 	const session = await auth.api.getSession({
@@ -119,12 +123,7 @@ export const getPostById = handler(async (req: Request, res: Response) => {
 		},
 	})
 
-	const post = await db
-		.select()
-		.from(schema.posts)
-		.where(eq(schema.posts.id, postId))
-		.limit(1)
-		.then((post) => post[0])
+	const post = await getPostByPostId(postId, session)
 
 	if (!post) {
 		logger.error('Failed to get post', { post })
@@ -136,80 +135,8 @@ export const getPostById = handler(async (req: Request, res: Response) => {
 		return
 	}
 
-	const copyPost = { ...post } as Partial<
-		PostResponse & {
-			user: {
-				username: string | null
-				image: string | null
-				about_description: string | null
-
-				postsCount: number
-				commentsCount: number
-
-				createdAt: Date | null
-			}
-		}
-	>
-
-	const upvotesCount = await db.$count(schema.upvotes, eq(schema.upvotes.postId, post.id))
-	const downvotesCount = await db.$count(schema.downvotes, eq(schema.downvotes.postId, post.id))
-
-	const likes = (upvotesCount || 0) - (downvotesCount || 0) // Calculate the likes count
-	const comments = await db.select().from(schema.comments).where(eq(schema.comments.postId, post.id))
-
-	copyPost.likesCount = likes
-	copyPost.commentsCount = comments.length
-
-	const user = await db
-		.select()
-		.from(schema.user)
-		.where(eq(schema.user.id, post.userId))
-		.limit(1)
-		.then((user) => user[0])
-
-	const commentsCount = await db.$count(schema.comments, eq(schema.comments.userId, user.id))
-	const postsCount = await db.$count(schema.posts, eq(schema.posts.userId, user.id))
-
-	if (session && session.user) {
-		const userIdString = session.user.id
-
-		const upvoted = await db
-			.select()
-			.from(schema.upvotes)
-			.where(and(eq(schema.upvotes.postId, postId), eq(schema.upvotes.userId, userIdString)))
-			.limit(1)
-			.then((upvotes) => {
-				return upvotes[0]
-			})
-
-		const downvoted = await db
-			.select()
-			.from(schema.downvotes)
-			.where(and(eq(schema.downvotes.postId, postId), eq(schema.downvotes.userId, userIdString)))
-			.limit(1)
-			.then((downvotes) => {
-				return downvotes[0]
-			})
-
-		copyPost.upvoted = upvoted ? true : false
-		copyPost.downvoted = downvoted ? true : false
-	}
-
-	delete copyPost.userId
-
-	copyPost.user = {
-		username: user.username,
-		image: user.image,
-		about_description: user.about_description,
-
-		postsCount: postsCount,
-		commentsCount: commentsCount,
-
-		createdAt: user.createdAt,
-	}
-
 	res.status(200).json({
-		data: copyPost,
+		data: post,
 	})
 })
 
