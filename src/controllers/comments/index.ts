8f1c301394fe31acfe96627425838503a64445fc
Robert Vilcu <robertvilcu89@gmail.com>
2025-05-13 00:36:53 +0100

    fix: issue with comment not found & removed unused controller function

--- src/controllers/comments/index.ts
+++ src/controllers/comments/index.ts
@@ -136,7 +136,7 @@ export const getParentCommentByComment = handler(async (req: Request, res: Respo
 		},
 	})
 
-	const commentToSearch = await db
+	const commentToSearchParentId = await db
 		.select()
 		.from(schema.comments)
 		.where(eq(schema.comments.id, commentId))
@@ -149,27 +149,23 @@ export const getParentCommentByComment = handler(async (req: Request, res: Respo
 				return null
 			}
 		})
+	let comment = commentToSearchParentId
 
-	if (!commentToSearch) {
-		res.status(404).json({
-			data: 'Comment not found',
-		})
-		return
+	if (commentToSearchParentId?.parentId) {
+		comment = await db
+			.select()
+			.from(schema.comments)
+			.where(eq(schema.comments.id, commentToSearchParentId.parentId))
+			.limit(1)
+			.then((comments) => {
+				if (comments.length > 0) {
+					const comment = comments[0]
+					return comment
+				} else {
+					return null
+				}
+			})
 	}
-
-	const comment = await db
-		.select()
-		.from(schema.comments)
-		.where(eq(schema.comments.id, commentToSearch.parentId as string))
-		.limit(1)
-		.then((comments) => {
-			if (comments.length > 0) {
-				const comment = comments[0]
-				return comment
-			} else {
-				return null
-			}
-		})
 
 	if (!comment) {
 		res.status(404).json({
@@ -204,7 +200,7 @@ export const getParentCommentByComment = handler(async (req: Request, res: Respo
 	const user = await db
 		.select()
 		.from(schema.user)
-		.where(eq(schema.user.id, commentToSearch.userId))
+		.where(eq(schema.user.id, comment.userId))
 		.limit(1)
 		.then((user) => {
 			return user[0]
@@ -253,119 +249,6 @@ export const getParentCommentByComment = handler(async (req: Request, res: Respo
 	res.status(200).json({
 		postData: post,
 		commentData: newComment,
-	})
-})
-
-export const getCommentsByPostAndComment = handler(async (req: Request, res: Response) => {
-	const { postId, commentId } = req.params
-	const { page } = req.query
-
-	const limit = 3
-	const offset = (Number(page) - 1) * limit
-
-	const session = await auth.api.getSession({
-		headers: fromNodeHeaders(req.headers),
-		query: {
-			disableCookieCache: true,
-		},
-	})
-
-	let arrayComments = [] as Partial<
-		schema.Comments & {
-			user: {
-				username: string | null
-				image: string | null
-			}
-			upvoted?: boolean
-			downvoted?: boolean
-		}
-	>[]
-
-	const comments = await db
-		.select()
-		.from(schema.comments)
-		.where(and(eq(schema.comments.postId, postId), eq(schema.comments.parentId, commentId)))
-		.orderBy(desc(schema.comments.createdAt))
-		.limit(limit + 1)
-		.offset(offset)
-
-	if (comments.length > 0) {
-		arrayComments = await Promise.all(
-			comments.map(async (comment: schema.Comments) => {
-				const newComment = { ...comment } as Partial<
-					schema.Comments & {
-						user: {
-							username: string | null
-							image: string | null
-						}
-
-						upvoted?: boolean
-						downvoted?: boolean
-
-						replyCount: number
-					}
-				>
-
-				const user = await db
-					.select()
-					.from(schema.user)
-					.where(eq(schema.user.id, comment.userId))
-					.limit(1)
-					.then((user) => {
-						return user[0]
-					})
-
-				delete newComment.userId
-
-				newComment.user = {
-					username: user.username,
-					image: user.image,
-				}
-
-				if (session && session.user) {
-					const userIdString = session.user.id
-
-					const upvote = await db
-						.select()
-						.from(schema.comments_upvotes)
-						.where(and(eq(schema.comments_upvotes.commentId, comment.id), eq(schema.comments_upvotes.userId, userIdString)))
-						.limit(1)
-						.then((upvotes) => {
-							return upvotes[0]
-						})
-
-					const downvote = await db
-						.select()
-						.from(schema.comments_downvotes)
-						.where(and(eq(schema.comments_downvotes.commentId, comment.id), eq(schema.comments_downvotes.userId, userIdString)))
-						.limit(1)
-						.then((downvotes) => {
-							return downvotes[0]
-						})
-
-					newComment.upvoted = upvote ? true : false
-					newComment.downvoted = downvote ? true : false
-				}
-
-				newComment.replyCount = await db
-					.select()
-					.from(schema.comments)
-					.where(eq(schema.comments.parentId, comment.id))
-					.then((replies) => {
-						return replies.length
-					}) // Count the number of replies to the comment
-
-				return newComment
-			})
-		)
-	}
-
-	const hasNextPage = comments.length > limit
-	const paginatedComments = hasNextPage ? arrayComments.slice(0, limit) : arrayComments
-
-	res.status(200).json({
-		data: paginatedComments,
-		nextCursor: hasNextPage ? Number(page) + 1 : undefined,
 	})
 })
 
